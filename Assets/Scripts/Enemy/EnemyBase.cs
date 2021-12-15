b5f4280513bba3bb7ddd602aebf698530155f8de
dmdylan <51095343+dmdylan@users.noreply.github.com>
2021-12-14 23:45:52 -0500

    working with nodecanvas

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -1,18 +1,27 @@
+using NodeCanvas.BehaviourTrees;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class EnemyBase : MonoBehaviour
+public class EnemyBase : MonoBehaviour
 {
     [SerializeField] protected EnemyStats stats;
     protected event Action<float> DamageTaken;
     protected float currentHealth;
+    private BehaviourTreeOwner behaviourTree;
+
+    private void Awake()
+    {
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = stats.MaxHealth;
+        //behaviourTree = GetComponent<BehaviourTreeOwner>();
+        //behaviourTree.StartBehaviour(behaviourTree.behaviour);
     }
 
     // Update is called once per frame
@@ -41,5 +50,8 @@ public abstract class EnemyBase : MonoBehaviour
         }
     }
 
-    protected abstract void Die();
+    protected void Die()
+    {
+
+    }
 }
